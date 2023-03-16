b9d20bcdbce7d0bb019800d4b8a2a5b01c9b369f
stanw0ng <user.stanley@gmail.com>
2023-03-16 18:41:35 -0400

    added iss.js and index.js to run it

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-// index.js
-const { fetchMyIP } = require('./iss');
+// iss part 1
+// const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// iss part 2
+const { fetchCoordsByIP } = require('./iss');
+const ip = '97.108.183.55'
+
+fetchCoordsByIP(ip, (error, coordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned coordinates:' , coordinates);
 });
--- iss.js
+++ iss.js
@@ -1,26 +1,56 @@
+const { parse } = require('path');
 const request = require('request');
 
-const fetchMyIP = function(callback) {
+// IPIFY API
+// const fetchMyIP = function(callback) {
   
-  const url = `https://api.ipify.org/?format=json`;
+//   const url = `https://api.ipify.org/?format=json`;
   
-  request(url, (error, response, body) => {
+//   request(url, (error, response, body) => {
     
-    //failures from
+//     //failures from
+//     if (error) {
+//       return callback(error, null);
+//     }
+
+//     if (response.statusCode !== 200) {
+//       const errorMsg = `Invalid status code: ${response.statusCode}`;
+//       callback(errorMsg, null);
+//       return;
+//     }
+
+//     // successful request
+//     const fetchedIP = JSON.parse(body).ip;
+//     callback(null, fetchedIP);
+//   });
+// };
+
+// ipwhois API
+const fetchCoordsByIP = function (ip, callback) {
+  
+  const url = `http://ipwho.is/` + ip
+
+  request(url + ip, (error, response, body) => {
+    
     if (error) {
-      return callback(error, null);
+      callback(error, null);
+      return
     }
 
-    if (response.statusCode !== 200) {
-      const errorMsg = `Invalid status code: ${response.statusCode}`;
+    // successful request
+    const parsedBody = JSON.parse(body);
+
+    if (!parse.body.success) {
+      const errorMsg = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
       callback(errorMsg, null);
       return;
     }
 
-    // successful request
-    const fetchedIP = JSON.parse(body).ip;
-    callback(null, fetchedIP);
+    const { latitude, longitude } = parsedBody;
+    callback(null, { latitude, longitude });
   });
-};
+}
 
-module.exports = { fetchMyIP };
+module.exports = {
+  fetchCoordsByIP
+};
