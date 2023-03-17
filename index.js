cda22a323aeab6cae9dac96ae7f2a24c53e85cf3
stanw0ng <user.stanley@gmail.com>
2023-03-16 23:50:26 -0400

    combined results of 3 APIs integrated in iss.js and running on index.js

--- index.js
+++ index.js
@@ -28,15 +28,37 @@
 //////////////---------
 
 // iss part 3
-const { fetchISSFlyOverTimes } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
 
-const coords = { latitude: 43.653226, longitude: -79.3831843 }
+// const coords = { latitude: 43.653226, longitude: -79.3831843 }
 
-fetchISSFlyOverTimes(coords, (error, passTimes) => {
+// fetchISSFlyOverTimes(coords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned flyover times:' , passTimes);
+// });
+
+//////////////---------
+
+// iss part 4
+const { nextISSTimesForMyLocation } = require('./iss'); 
+
+const formatPassTimes = function(passTimes) {
+  for (const time of passTimes) {
+    const datetime = new Date(0); //sets time to Epoch time (Jan 1, 1970, 00:00:00 UTC), the Date object autoformats "Day Mon DD YYYY HH:mm:ss GMT+ZZZZ (Timezone)"!
+    datetime.setUTCSeconds(time.risetime); //adds risetime to Epoch time giving a nice formatted exact date
+    const duration = time.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
 
-  console.log('It worked! Returned flyover times:' , passTimes);
+  formatPassTimes(passTimes);
 });
--- iss.js
+++ iss.js
@@ -2,56 +2,56 @@ const { parse } = require('path');
 const request = require('request');
 
 // IPIFY API
-// const fetchMyIP = function(callback) {
+const fetchMyIP = function(callback) {
   
-//   const url = `https://api.ipify.org/?format=json`;
+  const url = `https://api.ipify.org/?format=json`;
   
-//   request(url, (error, response, body) => {
+  request(url, (error, response, body) => {
     
-//     //failures from
-//     if (error) {
-//       return callback(error, null);
-//     }
+    //failures from
+    if (error) {
+      return callback(error, null);
+    }
 
-//     if (response.statusCode !== 200) {
-//       const errorMsg = `Invalid status code: ${response.statusCode}`;
-//       callback(errorMsg, null);
-//       return;
-//     }
+    if (response.statusCode !== 200) {
+      const errorMsg = `Invalid status code: ${response.statusCode}`;
+      callback(errorMsg, null);
+      return;
+    }
 
-//     // successful request
-//     const fetchedIP = JSON.parse(body).ip;
-//     callback(null, fetchedIP);
-//   });
-// };
+    // successful request
+    const fetchedIP = JSON.parse(body).ip;
+    callback(null, fetchedIP);
+  });
+};
 
 // module.exports = {
 //   fetchMyIP
 // };
 
-// ipwhois API
-// const fetchCoordsByIP = function(ip, callback) {
+// IPWHOIS API
+const fetchCoordsByIP = function(ip, callback) { 
 
-//   request(`http://ipwho.is/${ip}`, (error, response, body) => {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
     
-//     if (error) {
-//       callback(error, null);
-//       return;
-//     }
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-//     // successful request
-//     const data = JSON.parse(body);
+    // successful request
+    const data = JSON.parse(body);
 
-//     if (!data.success) {
-//       const errorMsg = `Success status was ${data.success}. Server message says: ${data.message} when fetching for IP ${data.ip}`;
-//       callback(errorMsg, null);
-//       return;
-//     }
+    if (!data.success) {
+      const errorMsg = `Success status was ${data.success}. Server message says: ${data.message} when fetching for IP ${data.ip}`;
+      callback(errorMsg, null);
+      return;
+    }
 
-//     const { latitude, longitude } = data;
-//     callback(null, { latitude, longitude });
-//   });
-// };
+    const { latitude, longitude } = data;
+    callback(null, { latitude, longitude });
+  });
+};
 
 // module.exports = {
 //   fetchCoordsByIP
@@ -77,10 +77,36 @@ const fetchISSFlyOverTimes = function(coords, callback) {
     }
 
     const passes = JSON.parse(body).response;
-    callback(null, passes)
+    callback(null, passes) // passes take a { risetime :28379474816, duration: 545  } format, number is in seconds
   });
 };
 
-module.exports = {
-  fetchISSFlyOverTimes
+// module.exports = {
+//   fetchISSFlyOverTimes
+// };
+
+// ULTIMATE BLUE EYES WHITE DRAGON --- TRIPLE CALLBACKS
+
+const nextISSTimesForMyLocation = function(callback) { //this works because the functions and requests have already been defined from above
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => { //notice how ip is passed down
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => { //notice how loc is passed down
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses); // notice how nextPasses is passed finally goes through
+      });
+    });
+  });
 };
+
+module.exports = { nextISSTimesForMyLocation };
